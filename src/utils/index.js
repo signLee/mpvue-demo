dd394a5812738937922df7e4e1a32dc4a4a92921
signLee <412245794@qq.com>
2019-05-13 10:27:05 +0800

    add:requret methods
    接口函数封装

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,6 +18,46 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+//-------------------------------------------------------------------------请求的封装
+
+const host = 'https://www.heyuhsuo.xyz/heyushuo';//接口请求的主机地址
+export {
+  host
+}
+//请求封装
+function request(url, method, data, header = {}) {
+  wx.showLoading({
+    title: '加载中' //数据请求前loading
+  })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: host + url, //仅为示例，并非真实的接口地址
+      method: method,
+      data: data,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        wx.hideLoading();
+        resolve(res.data)
+      },
+      fail: function (error) {
+        wx.hideLoading();
+        reject(false)
+      },
+      complete: function () {
+        wx.hideLoading();
+      }
+    })
+  })
+}
+export function get(url, data) {
+  return request(url, 'GET', data)
+}
+export function post(url, data) {
+  return request(url, 'POST', data)
+}
+
 export default {
   formatNumber,
   formatTime
